fb9a22bb98eae0dadd645ebbe5ebe10d31f1848b
Alex <dafen@ukr.net>
2022-04-18 19:54:57 +0300

    deploy again

--- manage-users-app.js
+++ manage-users-app.js
@@ -12,7 +12,9 @@ app.use(express.json())
 
 
 //app.use(cors())
+
 // CORS
+
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
